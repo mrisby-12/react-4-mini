8ed8b6638a176e3c0785c4ddaecb1c3b520e7e55
unknown <michellenrisby@gmail.com>
2018-03-06 12:03:19 -0600

    mini

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import routes from './routes';
+import { Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Link to='/'> <p>Route 1</p></Link>
+          <Link to='/2'> <p>Route 2</p></Link>
+          <Link to='/3'> <p>Route 3</p></Link>
+        </header>
+        { routes }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Route1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Route1() {
+    return(
+        <div>
+          Route 1 here!          
+        </div>
+    )
+}
+
+export default Route1;
